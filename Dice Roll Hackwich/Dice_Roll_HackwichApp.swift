305b63cdb98e6a7d2fc668bca4487719d8010324
Alex Anderson <andersonaj116@gmail.com>
2022-11-02 09:30:35 -0500

    Initial Commit

---
+++ Dice Roll Hackwich/Dice_Roll_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_Roll_HackwichApp.swift
+//  Dice Roll Hackwich
+//
+//  Created by Alex Anderson on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Dice_Roll_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
